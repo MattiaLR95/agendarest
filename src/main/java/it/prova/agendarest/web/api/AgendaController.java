ffe5f696ee42615efcfb7b5558a263e438453dbe
MattiaLR95 <laroccamattia@virgilio.it>
2022-11-28 23:41:03 +0100

    Implementazione della delete e dell'update

--- src/main/java/it/prova/agendarest/web/api/AgendaController.java
+++ src/main/java/it/prova/agendarest/web/api/AgendaController.java
@@ -7,9 +7,11 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -59,16 +61,16 @@ public class AgendaController {
 		return AgendaDTO.buildAgendaDTOFromModel(agendaInserita);
 
 	}
-	
+
 	@GetMapping("{/id}")
-	public AgendaDTO findById (@PathVariable(value = "id", required = true) Long id) {
+	public AgendaDTO findById(@PathVariable(value = "id", required = true) Long id) {
 		String username = SecurityContextHolder.getContext().getAuthentication().getName();
 		Utente utenteRichiedente = utenteService.findByUsername(username);
-		
+
 		Agenda agenda = agendaService.caricaSingoloElementoEager(id);
-		
-		if (utenteRichiedente.getRuoli().stream().filter(r -> r.getCodice().equals("ROLE_ADMIN")).findAny().
-				orElse(null) != null || utenteRichiedente.getId() == agenda.getUtente().getId()) {
+
+		if (utenteRichiedente.getRuoli().stream().filter(r -> r.getCodice().equals("ROLE_ADMIN")).findAny()
+				.orElse(null) != null || utenteRichiedente.getId() == agenda.getUtente().getId()) {
 			if (agenda == null)
 				throw new AgendaNotFoundException("Agenda not found con id: " + id);
 
@@ -76,5 +78,51 @@ public class AgendaController {
 		} else {
 			throw new PermessoNegatoException("non hai i permessi per visualizzare");
 		}
+	}
+
+	@PutMapping("/{id}")
+	public AgendaDTO update(@Valid @RequestBody AgendaDTO agendaInput, @PathVariable(required = true) Long id) {
+		String username = SecurityContextHolder.getContext().getAuthentication().getName();
+		Utente utenteLoggato = utenteService.findByUsername(username);
+
+		Agenda agenda = agendaService.caricaSingoloElemento(id);
+
+		if (agenda == null)
+			throw new AgendaNotFoundException("Agenda not found con id: " + id);
+
+		if (utenteLoggato.getRuoli().stream().filter(r -> r.getCodice().equals("ROLE_ADMIN")).findAny()
+				.orElse(null) != null || utenteLoggato.getId() == agenda.getUtente().getId()) {
+
+			agendaInput.setId(id);
+			agendaInput.setUtente(UtenteDTO.buildUtenteDTOFromModel(utenteService.findByUsername(username)));
+			Agenda agendaAggiornata = agendaService.aggiorna(agendaInput.buildAgendaModel());
+			return AgendaDTO.buildAgendaDTOFromModel(agendaAggiornata);
+
+		} else {
+			throw new PermessoNegatoException("Non hai i permessi per modificare questo elemento!");
+		}
+	}
+
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable(required = true) Long id) {
+
+		Agenda agenda = agendaService.caricaSingoloElemento(id);
+
+		if (agenda == null)
+			throw new AgendaNotFoundException("Agenda not found con id: " + id);
+
+		String username = SecurityContextHolder.getContext().getAuthentication().getName();
+		Utente utenteLoggato = utenteService.findByUsername(username);
+
+		if (utenteLoggato.getRuoli().stream().filter(r -> r.getCodice().equals("ROLE_ADMIN")).findAny()
+				.orElse(null) != null || utenteLoggato.getId() == agenda.getUtente().getId()) {
+
+			agendaService.rimuovi(id);
+
+		} else {
+			throw new PermessoNegatoException("Non hai i permessi per Eliminare questo elemento!");
+		}
+
 	}
 }
